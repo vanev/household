2f37d1400350ebc77c5044cadf2d977b54dc928e
Evan Siegel <siegel.evan@gmail.com>
2021-09-29 09:37:01 -0400

    Adds User Creation Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Provider as AuthProvider } from "./Authentication/hooks/useAuth";
 import Authentication from "./Authentication/components/Authentication";
+import UserCreation from "./User/componets/Creation";
 import styles from "./App.module.css";
 
 const App = () => (
   <AuthProvider>
     <div className={styles.App}>
       <Authentication />
+      <UserCreation />
     </div>
   </AuthProvider>
 );
---
+++ src/User/componets/Creation.tsx
@@ -0,0 +1,62 @@
+import { createUserWithEmailAndPassword } from "@firebase/auth";
+import { useState } from "react";
+import useAuth from "../../Authentication/hooks/useAuth";
+import Email from "../../Email";
+import firebaseAuth from "../../Firebase/auth";
+import Password from "../../Password";
+
+type Props = {
+  onSubmit: (email: Email, password: Password) => unknown;
+};
+
+const Form = ({ onSubmit }: Props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        onSubmit(email, password);
+      }}
+    >
+      <input
+        type="email"
+        placeholder="fake@email.net"
+        onChange={(event) => setEmail(event.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Passw0rd!"
+        onChange={(event) => setPassword(event.target.value)}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+const Creation = () => {
+  const auth = useAuth();
+
+  switch (auth._tag) {
+    case "Loading":
+    case "Authenticated":
+      return null;
+
+    case "Failed":
+    case "Unauthenticated":
+      return (
+        <div>
+          <Form
+            onSubmit={(email, password) => {
+              createUserWithEmailAndPassword(firebaseAuth, email, password);
+            }}
+          />
+        </div>
+      );
+  }
+};
+
+export default Creation;
