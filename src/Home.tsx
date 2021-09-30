fd9de07069f4a19586e853294a1bb1cafbcb8553
Evan Siegel <siegel.evan@gmail.com>
2021-09-30 19:00:39 -0400

    Adds All Todos List to Home Page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,3 +1,12 @@
-const Home = () => <p>Home</p>;
+import List from "./Todo/components/List";
+import todosCollection from "./Todo/queries/collection";
+import css from "./Home.module.css";
+
+const Home = () => (
+  <div className={css.root}>
+    <h1 className={css.title}>All Todos</h1>
+    <List query={todosCollection} className={css.list} />
+  </div>
+);
 
 export default Home;
