86ed939a7b3fb65dd8bc3b38d7d4a02733f8bb6d
Evan Siegel <siegel.evan@gmail.com>
2021-09-30 18:59:36 -0400

    Adds Todo Type

---
+++ src/Todo/types/Todo.ts
@@ -0,0 +1,14 @@
+import { isSome, Option } from "fp-ts/Option";
+import { NonEmptyString } from "../../NonEmptyString";
+
+export type Todo = {
+  title: NonEmptyString;
+  notes: string;
+  when: Option<Date>;
+  deadline: Option<Date>;
+  completedAt: Option<Date>;
+};
+
+export const isComplete = (todo: Todo): boolean => isSome(todo.completedAt);
+
+export default Todo;
