eb060889166abfe624e134fc217960b6c9054b82
Evan Siegel <siegel.evan@gmail.com>
2021-10-02 18:45:11 -0400

    Adds PublicRoute, PrivateRoute Components

---
+++ src/App/PublicRoute.tsx
@@ -0,0 +1,22 @@
+import { ComponentProps } from "react";
+import { Route, Redirect } from "react-router-dom";
+import useAuth from "../Authentication/hooks/useAuth";
+
+const PrivateRoute = ({ children, ...rest }: ComponentProps<typeof Route>) => {
+  const auth = useAuth();
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        auth._tag !== "Authenticated" ? (
+          children
+        ) : (
+          <Redirect to={{ pathname: "/", state: { from: location } }} />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
--- src/App/Routes.tsx
+++ src/App/Routes.tsx
@@ -2,16 +2,18 @@ import { Switch, Route } from "react-router-dom";
 import UsersCreate from "../pages/users/create";
 import Home from "../pages/index";
 import Everything from "../pages/everything";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 
 const Routes = () => (
   <Switch>
-    <Route path="/users/create">
+    <PublicRoute path="/users/create">
       <UsersCreate />
-    </Route>
+    </PublicRoute>
 
-    <Route path="/everything">
+    <PrivateRoute path="/everything">
       <Everything />
-    </Route>
+    </PrivateRoute>
 
     <Route path="/">
       <Home />
