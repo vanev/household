042c29840d1f273c4d9bd26415d222949471c2ad
Evan Siegel <siegel.evan@gmail.com>
2021-10-10 09:29:50 -0400

    Adds Observable.merge

--- src/Observable.ts
+++ src/Observable.ts
@@ -7,3 +7,11 @@ export type Observer<E, A> = {
 };
 
 export type Observable<E, A> = (observer: Observer<E, A>) => Unsubscribe;
+
+export const merge =
+  <E, A>(observables: Array<Observable<E, A>>): Observable<E, A> =>
+  (observer) => {
+    const unsubscribes = observables.map((observable) => observable(observer));
+
+    return () => unsubscribes.map((unsubscribe) => unsubscribe());
+  };
