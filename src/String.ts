d1948ccac48d61f3846ce4e78a87bc4533d05106
Evan Siegel <siegel.evan@gmail.com>
2021-10-10 22:01:14 -0400

    Moves NonEmptyString to lib

--- src/String.ts
+++ src/String.ts
@@ -1,4 +1,4 @@
-import { NonEmptyString } from "./NonEmptyString";
+import { NonEmptyString } from "./lib/NonEmptyString";
 
 export const isNonEmptyString = (str: string): str is NonEmptyString =>
   str.length !== 0;
