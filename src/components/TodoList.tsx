e50845a36fb6a380fe3633a156df19bb5ac6cc68
Evan Siegel <siegel.evan@gmail.com>
2021-10-15 08:32:27 -0400

    Sorts TodoList by Date

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,12 @@
+import { QueryDocumentSnapshot } from "@firebase/firestore";
 import classNames from "classnames";
-import { toArray } from "fp-ts/Map";
+import { sort } from "fp-ts/Array";
+import { Ord as dateOrd } from "fp-ts/Date";
+import { pipe } from "fp-ts/function";
+import { values } from "fp-ts/Map";
 import { Ord as stringOrd } from "fp-ts/string";
-import { none } from "fp-ts/Option";
+import { Option, isSome, getOrd, none } from "fp-ts/Option";
+import { contramap } from "fp-ts/Ord";
 import FirestoreObservable from "lib/Firebase/FirestoreObservable";
 import useExpandable from "hooks/useExpandable";
 import useTodos from "hooks/useTodos";
@@ -10,6 +15,23 @@ import ExpandedTodo from "./ExpandedTodo";
 import TodoItem from "./TodoItem";
 import css from "./TodoList.module.css";
 
+const snapshotId = (snapshot: QueryDocumentSnapshot<Todo>): string =>
+  snapshot.id;
+
+const ordId = contramap(snapshotId)(stringOrd);
+
+const todoDate = (snapshot: QueryDocumentSnapshot<Todo>): Option<Date> => {
+  const todo = snapshot.data();
+
+  if (isSome(todo.completedAt)) return todo.completedAt;
+
+  if (isSome(todo.when)) return todo.when;
+
+  return todo.deadline;
+};
+
+const todoDateOrd = contramap(todoDate)(getOrd(dateOrd));
+
 type ListProps = {
   observable: FirestoreObservable<Todo>;
   className?: string;
@@ -17,11 +39,15 @@ type ListProps = {
 
 const List = ({ observable, className }: ListProps) => {
   const [isExpanded, expand, close] = useExpandable();
-  const snapshots = useTodos(observable);
+  const snapshots = pipe(
+    useTodos(observable),
+    values(ordId),
+    sort(todoDateOrd),
+  );
 
   return (
     <div className={classNames(css.root, className)}>
-      {toArray(stringOrd)(snapshots).map(([_, snapshot]) => (
+      {snapshots.map((snapshot) => (
         <TodoItem
           key={snapshot.id}
           className={css.item}
