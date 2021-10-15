1208ce8d46bd05bd2a003473237a44f6b42a93d2
Evan Siegel <siegel.evan@gmail.com>
2021-10-15 08:12:32 -0400

    Adds doAt, doAtIs, dotAtIsBefore, doAtIsDuring

--- src/types/Todo.ts
+++ src/types/Todo.ts
@@ -1,5 +1,11 @@
-import { isSome, Option } from "fp-ts/Option";
+import { isBefore as dateIsBefore } from "date-fns/fp";
+import { Ord as dateOrd } from "fp-ts/Date";
+import { flow } from "fp-ts/function";
 import { NonEmptyString } from "lib/NonEmptyString";
+import { Option, isSome, map, getOrElse, getOrd } from "fp-ts/Option";
+import { Ord, between, contramap } from "fp-ts/Ord";
+import { Predicate } from "fp-ts/Predicate";
+import { DateRange } from "lib/DateRange";
 
 export type Todo = {
   title: NonEmptyString;
@@ -10,5 +16,22 @@ export type Todo = {
 };
 
 export const isComplete = (todo: Todo): boolean => isSome(todo.completedAt);
+
+export const doAt = (todo: Todo): Option<Date> =>
+  isSome(todo.when) ? todo.when : todo.deadline;
+
+export const doAtIs = (predicate: Predicate<Date>) =>
+  flow(
+    doAt,
+    map(predicate),
+    getOrElse(() => false),
+  );
+
+export const doAtIsBefore = (target: Date) => doAtIs(dateIsBefore(target));
+
+export const doAtIsDuring = ([start, end]: DateRange) =>
+  doAtIs(between(dateOrd)(start, end));
+
+export const ordDoAt: Ord<Todo> = contramap(doAt)(getOrd(dateOrd));
 
 export default Todo;
